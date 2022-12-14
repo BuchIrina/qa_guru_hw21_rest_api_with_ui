11d584b3a50f54ab07563ef58d0c5ecba748f4a1
Irina Buchneva <buchneva.irina986@gmail.com>
2022-12-14 11:26:21 +0300

    added homework test with checking the creation of test case steps

---
+++ src/test/java/com/buchneva/models/CreateTestCaseSteps.java
@@ -0,0 +1,10 @@
+package com.buchneva.models;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateTestCaseSteps {
+    public List<Step> steps;
+}
---
+++ src/test/java/com/buchneva/models/Step.java
@@ -0,0 +1,8 @@
+package com.buchneva.models;
+
+import lombok.Data;
+
+@Data
+public class Step {
+    public String name;
+}
